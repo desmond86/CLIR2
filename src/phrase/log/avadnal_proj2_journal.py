59f0a63d00a22bf35a738d4e72e9bd196dbbc99a
avadnal <worked13@gmail.com>
2012-06-24 18:29:47 +1000

    Update journal

--- src/phrase/log/avadnal_proj2_journal.py
+++ src/phrase/log/avadnal_proj2_journal.py
@@ -40,4 +40,21 @@ the koehn-06 Decoding chapter pg 165, 170. Setting up an overall structure "deco
 scaffolding - ie. the functions which have not yet been implemented. Spent time working on histogram and threshold pruning
 in addition to the section on determing future cost for any input span. Did not have access to model data (translation/language)
 while doing this, which was to be committed at the end of the day.
+
+Hour 17-19. Merging code (both team members work from uni - proxy doesn't allow
+push/pull/commits to github), running tests with LM/TM code in separate file.
+
+Hour 20-22. Determining how to incorporate the LM/TM code into the pruning process based on tests performed
+on the separate file. Begin substituting dummy data with the real model data in test file.
+
+Hour 23-29. Figuring out how to extract the correct information for a given sentence. I wasn't sure if what I had
+was working or not, so as a means of testing I used the dummy data from koehn-06 to reconstruct and replicate
+their experiment. If this was successful, I would know my approach was correct. Seems to be working, ie. replicating
+the future cost table as per specified in the koehn-06 errata. Next step is to merge all test code into a more concrete
+implementation, that is - using all obtained costs from the model. (FC/TM/LM)
+
+Hour 30-37. Finished implementation of future cost estimation based on a given input phrase. TM/LM costs are also
+being correctly extracted from the models. Adding comments to functions and also deleting redundant code. Next step
+is to use all these phrasal costs in the threshold pruning process.
+
 '''
