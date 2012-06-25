90b1d80daeec90196f2faab705318fd62bcdb498
avadnal <worked13@gmail.com>
2012-06-25 13:25:42 +1000

    Introduced a decoder class within decoder.py

--- src/phrase/decoder.py
+++ src/phrase/decoder.py
@@ -6,10 +6,16 @@
 """
 Assignment 2. A Phrase-based translation model and decoder.
 
->>> decoder_test(5)
+>>> all_file = "source_files/all.lm"
+>>> e_file = "source_files/all.lowercased.raw.en" 
+>>> f_file = "source_files/all.lowercased.raw.fr"
+>>> a_file = "source_files/aligned.grow-diag-final-and"
+>>> max_stack_size = 10
+>>> decoder = Decoder(all_file, e_file, f_file, a_file, max_stack_size)
+>>> decoder.process_models()
+>>> decoder.decoder_test(f_file, 5)
 
 """
-
 from __future__ import division
 from pprint import pprint
 
@@ -19,152 +25,171 @@ from ReorderingModel import ReorderingModel
 from datastructures import Hypothesis, Stack
 from utils import get_trans_opts
 
-MAX_STACK_SIZE = 10
+class Decoder:
 
-#############################################
-# Model processing
-#############################################
+    def __init__(self, all_file, e_file, f_file, align_file, max_stack_size):
+        self.all_file = all_file
+        self.english_file = e_file
+        self.foreign_file = f_file
+        self.alignment_file = align_file
+        self.MAX_STACK_SIZE = max_stack_size
+        self.lm = SRILangModel()
+        self.rm = ReorderingModel()
+        self.tm = TranslationModel(self.english_file, self.foreign_file,
+                                    self.alignment_file)
 
-lm = SRILangModel()
-rm = ReorderingModel()
+    def process_models(self):
+        self.lm.read_lm_file(self.all_file)
+        self.tm.extract()
 
-#read language model file
-lm.read_lm_file("source_files/all.lm")
+    # MAX_STACK_SIZE = 10
 
-english_file = "source_files/all.lowercased.raw.en"
-foreign_file = "source_files/all.lowercased.raw.fr"
-alignment_file = "source_files/aligned.grow-diag-final-and"
+    #############################################
+    # Model processing
+    #############################################
 
-#run the translation model
-tm = TranslationModel(english_file, foreign_file, alignment_file)
-tm.extract()
+    # lm = SRILangModel()
+    # rm = ReorderingModel()
 
+    # #read language model file
+    # lm.read_lm_file("source_files/all.lm")
 
-def get_tm_info(sent):
-    """
-    Gets all the required data from the translation model. A greedy
-    approach was taken, where in a sorted list of translation probabilities
-    the 'best' probability is chosen and returned.
+    # english_file = "source_files/all.lowercased.raw.en"
+    # foreign_file = "source_files/all.lowercased.raw.fr"
+    # alignment_file = "source_files/aligned.grow-diag-final-and"
 
-    A sentence in this case is perceived as containing one or
-    more words, ie. a phrase.
-
-    Input: sent - A sentence
-    Output: tm_dict - The translation model dictionary
-            best_score - The best score for a translation
-    """
-
-    if isinstance(sent, list):
-        s = ' '.join(sent)
-
-    # just in case a string is passed in by 'accident'
-    else:
-        s = sent
-
-    tm_dict = tm.get_translation_model_prob_e(s)
-
-    # sorted_list[0] = best scoring (entry, prob)
-    #   - first entry in sorted trans prob table
-    # sorted_list[-1] = worst scoring (entry, prob)
-    #   - last entry in sorted trans prob table
-    sorted_list = sorted(tm_dict.iteritems(), key=lambda (k, v): (v, k),
-                    reverse=True)
-
-    # If there is data to process, get the best score
-    # Otherwise there is 'no' best score because an empty list was received
-    if sorted_list != []:
-        best_score = float(sorted_list[0][1])
-
-    else:
-        best_score = None
-
-    return best_score
+    # #run the translation model
+    # tm = TranslationModel(english_file, foreign_file, alignment_file)
+    # tm.extract()
 
 
-def get_future_cost_table(sent):
-    """
-    Used to get the future cost of processing words/phrases from
-    a given sentence. This takes into account the translation model's score.
-    A table is generated similar to koehn-06 page 171. Keys are the
-    words/phrases, values are their corresponding future costs.
+    def get_tm_info(self, sent):
+        """
+        Gets all the required data from the translation model. A greedy
+        approach was taken, where in a sorted list of translation probabilities
+        the 'best' probability is chosen and returned.
 
-    Future cost ignores reordering model.
+        A sentence in this case is perceived as containing one or
+        more words, ie. a phrase.
 
-    Input: sent - A sentence
-    Output: A future cost table
+        Input: sent - A sentence
+        Output: tm_dict - The translation model dictionary
+                best_score - The best score for a translation
+        """
 
-    """
-    fc_table = defaultdict(lambda: defaultdict(None))
+        if isinstance(sent, list):
+            s = ' '.join(sent)
 
-    for length in range(1, len(sent) + 1):
-        for start in range(0, len(sent) + 1 - length):
-            end = start + length
-            fc_table[start][end] = float('-inf')
+        # just in case a string is passed in by 'accident'
+        else:
+            s = sent
 
-            key1 = ' '.join(sent[start:end])
-            best_score = get_tm_info(key1)
-            if best_score is not None:
-                fc_table[start][end] = best_score
+        tm_dict = self.tm.get_translation_model_prob_e(s)
 
-            for i in range(start, end - 1):
+        # sorted_list[0] = best scoring (entry, prob)
+        #   - first entry in sorted trans prob table
+        # sorted_list[-1] = worst scoring (entry, prob)
+        #   - last entry in sorted trans prob table
+        sorted_list = sorted(tm_dict.iteritems(), key=lambda (k, v): (v, k),
+                        reverse=True)
 
-                # The cheapest cost estimate for a span is either the cheapest
-                # cost for a translation option or the cheapest sum of costs
-                # for a pair of spans that cover it completely
-                if (fc_table[start][i + 1] +
-                    fc_table[i + 1][end]) > fc_table[start][end]:
-                    fc_table[start][end] = (fc_table[start][i + 1] +
-                        fc_table[i + 1][end])
+        # If there is data to process, get the best score
+        # Otherwise there is 'no' best score because an empty list was received
+        if sorted_list != []:
+            best_score = float(sorted_list[0][1])
 
-    return fc_table
+        else:
+            best_score = None
+
+        return best_score
 
 
-def decoder_test(n_sentences):
-    """
-    Used to get the future cost of processing words/phrases from
-    a given sentence. This takes into account the translation model's score.
-    A table is generated similar to koehn-06 page 171. Keys are the
-    words/phrases, values are their corresponding future costs.
+    def get_future_cost_table(self, sent):
+        """
+        Used to get the future cost of processing words/phrases from
+        a given sentence. This takes into account the translation model's score.
+        A table is generated similar to koehn-06 page 171. Keys are the
+        words/phrases, values are their corresponding future costs.
 
-    Future cost ignores reordering model.
+        Future cost ignores reordering model.
 
-    Input: n_sentences - The number of sentences to parse from the
-           foreign file
-    Output: The corresponding English/Foreign sentences and its associated
-           processing cost
+        Input: sent - A sentence
+        Output: A future cost table
 
-    """
-    
-    f = open(foreign_file, 'r')
-    lines = []
-    [lines.append(line.split("\n")) for line in f.readlines()[:n_sentences]]
+        """
+        fc_table = defaultdict(lambda: defaultdict(None))
 
-    for i in range(len(lines)):
-  
-        print 'Translating phrase %d of %d\n.' % (i+1, len(lines))
-        input_sent = lines[i][0].split(' ')
-        fc_table = get_future_cost_table(input_sent)
+        for length in range(1, len(sent) + 1):
+            for start in range(0, len(sent) + 1 - length):
+                end = start + length
+                fc_table[start][end] = float('-inf')
 
-        stacks = [Stack(MAX_STACK_SIZE) for x in range(len(input_sent) + 1)]
+                key1 = ' '.join(sent[start:end])
+                best_score = self.get_tm_info(key1)
+                if best_score is not None:
+                    fc_table[start][end] = best_score
 
-        empty_hyp = Hypothesis(None, None, input_sent, fc_table)
-        stacks[0].add(empty_hyp)
+                for i in range(start, end - 1):
 
-        for idx, stack in enumerate(stacks):
-            for hyp in stack.hypotheses():
-                for trans_opt in get_trans_opts(hyp, tm, rm, lm):
-                    new_hyp = Hypothesis(hyp, trans_opt)
-                    new_stack = stacks[new_hyp.input_len()]
-                    new_stack.add(new_hyp)
+                    # The cheapest cost estimate for a span is either the
+                    # cheapest cost for a translation option or the cheapest
+                    # sum of costs for a pair of spans that cover it completely
+                    if (fc_table[start][i + 1] +
+                        fc_table[i + 1][end]) > fc_table[start][end]:
+                        fc_table[start][end] = (fc_table[start][i + 1] + \
+                            fc_table[i + 1][end])
 
-    last_stack = stacks[-1]
-    best_hyp = last_stack.best()
-    translation = best_hyp.trans['output']
+        return fc_table
 
-    if best_hyp is not None:
-        print best_hyp.trans['input']
-        print best_hyp.trans['output']
-        print best_hyp.trans['score']
+
+    def decoder_test(self, foreign_file, n_sentences):
+        """
+        Used to get the future cost of processing words/phrases from
+        a given sentence. This takes into account the translation model's score.
+        A table is generated similar to koehn-06 page 171. Keys are the
+        words/phrases, values are their corresponding future costs.
+
+        Future cost ignores reordering model.
+
+        Input: n_sentences - The number of sentences to parse from the
+               foreign file
+        Output: The corresponding English/Foreign sentences and its associated
+               processing cost
+
+        """
+        
+        f = open(foreign_file, 'r')
+        lines = []
+        [lines.append(line.split("\n")) for line in f.readlines()[:n_sentences]]
+
+        for i in range(len(lines)):
+      
+            print 'Translating phrase %d of %d\n.' % (i+1, len(lines))
+            input_sent = lines[i][0].split(' ')
+            fc_table = self.get_future_cost_table(input_sent)
+
+            stacks = [Stack(self.MAX_STACK_SIZE)
+             for x in range(len(input_sent) + 1)]
+
+            empty_hyp = Hypothesis(None, None, input_sent, fc_table)
+            stacks[0].add(empty_hyp)
+
+            for idx, stack in enumerate(stacks):
+                for hyp in stack.hypotheses():
+                    for trans_opt in get_trans_opts(hyp, self.tm,
+                                                     self.rm, self.lm):
+                        new_hyp = Hypothesis(hyp, trans_opt)
+                        new_stack = stacks[new_hyp.input_len()]
+                        new_stack.add(new_hyp)
+
+            last_stack = stacks[-1]
+            best_hyp = last_stack.best()
+            translation = best_hyp.trans['output']
+
+            if best_hyp is not None:
+                print best_hyp.trans['input']
+                print best_hyp.trans['output']
+                print best_hyp.trans['score']
 
 if __name__ == '__main__':
     import doctest
